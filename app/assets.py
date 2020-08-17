9965e7b48b2ba37a9b5f687d63edaad927f4f78e
Raihan Stark <raihanyudosaputra30@gmail.com>
2020-08-18 02:30:58 +0700

    add flask-assets filters

--- app/assets.py
+++ app/assets.py
@@ -3,5 +3,8 @@ from flask_assets import Bundle
 bundles = {
     'app_js': Bundle(
         'js/app.js',
-        output='gen/home.js'),
+        output='dist/app.js',filters="jsmin"),
+    'app_scss': Bundle(
+        'scss/app.scss',
+        output='dist/app.css',filters="pyscss",),
 }
