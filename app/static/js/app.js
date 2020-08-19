a5fe5e5e8ee9ee9dd119b7d54c2d6a181d49108d
Raihan Stark <raihanyudosaputra30@gmail.com>
2020-08-19 07:04:37 +0700

    add predefine recent comment

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -126,17 +126,25 @@ function pool_comments() {
       $(".chat-list").empty();
 
       const reversed_comments = response.comments.reverse();
-      reversed_comments.forEach((e) => {
-        $(".chat-list").append(
-          `
+      if (reversed_comments.length === 0) {
+        $(".chat-list").append(`
         <li>
-          <div class="chat-content ">
-            <h5>${e.user.username}</h5>
-            <div class="box bg-light-info">${e.text}</div>
-          </div>
-        </li>`
-        );
-      });
+              No recent comments
+            </li>
+        `);
+      } else {
+        reversed_comments.forEach((e) => {
+          $(".chat-list").append(
+            `
+          <li>
+            <div class="chat-content ">
+              <h5>${e.user.username}</h5>
+              <div class="box bg-light-info">${e.text}</div>
+            </div>
+          </li>`
+          );
+        });
+      }
     },
     complete: function () {
       if (is_live_now) {
