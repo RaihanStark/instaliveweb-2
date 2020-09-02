ec6c21fc99cd62308db15b58f7e764d1094d9910
Raihan Stark <raihanyudosaputra30@gmail.com>
2020-09-02 11:37:23 +0700

    update config system

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
 
     VERSION = '1.4.0'
@@ -11,7 +15,12 @@ class Config:
         print('SECRET KEY ENV VAR NOT SET! SHOULD NOT SEE IN PRODUCTION')
 
 class DevelopmentConfig(Config):
+
     DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL',
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite'))
+
     @classmethod
     def init_app(cls, app):
         print('THIS APP IS IN DEBUG MODE. \
